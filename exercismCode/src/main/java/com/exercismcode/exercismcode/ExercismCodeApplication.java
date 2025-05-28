e02ebd7893452856ea83cf446ead41f475caaf49
rych <rychraya@gmail.com>
2025-05-28 17:54:39 -0600

    Primer commit

---
+++ exercismCode/src/main/java/com/exercismcode/exercismcode/ExercismCodeApplication.java
@@ -0,0 +1,13 @@
+package com.exercismcode.exercismcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercismCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercismCodeApplication.class, args);
+	}
+
+}
---
+++ exercismCode/src/test/java/com/exercismcode/exercismcode/ExercismCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercismcode.exercismcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercismCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
