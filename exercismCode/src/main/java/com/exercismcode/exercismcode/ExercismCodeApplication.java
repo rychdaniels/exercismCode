f7454e11d5d2bae90240e555f733389460ca184b
rych <rychraya@gmail.com>
2025-05-30 18:31:44 -0600

    Ejercicio numero 1 Greeter

--- exercismCode/src/main/java/com/exercismcode/exercismcode/ExercismCodeApplication.java
+++ exercismCode/src/main/java/com/exercismcode/exercismcode/ExercismCodeApplication.java
@@ -1,5 +1,6 @@
 package com.exercismcode.exercismcode;
 
+import com.exercismcode.exercismcode.greeter.Greeter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class ExercismCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExercismCodeApplication.class, args);
+
+		Greeter greeter = new Greeter();
+		greeter.getGreeting();
 	}
 
 }
---
+++ exercismCode/src/main/java/com/exercismcode/exercismcode/greeter/Greeter.java
@@ -0,0 +1,9 @@
+package com.exercismcode.exercismcode.greeter;
+
+public class Greeter {
+
+    public void getGreeting() {
+        System.out.println("Hello, World!");
+    }
+}
+
