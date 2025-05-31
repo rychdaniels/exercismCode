562000aeac9a8bba90d153e69ffecc0839f4ca33
rych <rychraya@gmail.com>
2025-05-30 20:31:13 -0600

    Ejercicio numero 4 BirdWatcher

---
+++ exercismCode/src/main/java/com/exercismcode/exercismcode/birdWatcher/BirdWatcher.java
@@ -0,0 +1,36 @@
+package com.exercismcode.exercismcode.birdWatcher;
+
+public class BirdWatcher {
+
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+    public int[] getLastWeek() {
+        return this.birdsPerDay;
+    }
+    public int getToday() {
+        return this.birdsPerDay[this.birdsPerDay.length-1];
+    }
+    public void incrementTodaysCount() {
+        this.birdsPerDay[this.birdsPerDay.length-1] += 1;
+    }
+    public boolean hasDayWithoutBirds() {
+        return Arrays.stream(birdsPerDay)
+                .filter(n -> n == 0)
+                .toArray()
+                .length > 0;
+    }
+    public int getCountForFirstDays(int numberOfDays) {
+        return Arrays.stream(birdsPerDay)
+                .limit(numberOfDays)
+                .reduce(0, (subtotal, element) -> subtotal + element);
+    }
+    public int getBusyDays() {
+        return Arrays.stream(birdsPerDay)
+                .filter(n -> n >= 5)
+                .toArray()
+                .length;
+    }
+}
